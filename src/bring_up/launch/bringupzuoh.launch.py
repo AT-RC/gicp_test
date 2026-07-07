29babf63ee6017d4ce5d968970a99ea0546e6757
xjh <hser93841@gmail.com>
2026-07-07 11:00:52 +0800

    修改地图名字

--- src/bring_up/launch/bringupzuoh.launch.py
+++ src/bring_up/launch/bringupzuoh.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
 
     declare_prior_pcd_file_cmd = DeclareLaunchArgument(
         "prior_pcd_file",
-        default_value=PathJoinSubstitution([point_lio_dir, "PCD", "zuoh.pcd"]),
+        default_value=PathJoinSubstitution([point_lio_dir, "PCD", "zuo.pcd"]),
         description="Full path to prior PCD file to load",
     )
 
--- src/bring_up/launch/taskzuoh.launch.py
+++ src/bring_up/launch/taskzuoh.launch.py
@@ -45,7 +45,7 @@ def generate_launch_description():
 
     declare_prior_pcd_file_cmd = DeclareLaunchArgument(
         "prior_pcd_file",
-        default_value=PathJoinSubstitution([point_lio_dir, "PCD", "taskzuoh.pcd"]),
+        default_value=PathJoinSubstitution([point_lio_dir, "PCD", "taskzuo.pcd"]),
         description="Full path to prior PCD file to load",
     )
 
